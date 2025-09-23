fc7a57e1f7a439c77e8b9c745f7b91cb0660d4e4
Erik <erik@apption.com>
2025-09-23 10:52:03 -0400

    added code suggestions for better checkboxes

--- docling_core/transforms/serializer/markdown.py
+++ docling_core/transforms/serializer/markdown.py
@@ -141,9 +141,9 @@ class MarkdownTextSerializer(BaseModel, BaseTextSerializer):
             processing_pending = True
 
         if item.label == DocItemLabel.CHECKBOX_SELECTED:
-            text = f"[x] {text}"
+            text = f"- [x] {text}"
         if item.label == DocItemLabel.CHECKBOX_UNSELECTED:
-            text = f"[ ] {text}"        
+            text = f"- [ ] {text}"        
         if isinstance(item, (ListItem, TitleItem, SectionHeaderItem)):
             if not has_inline_repr:
                 # case where processing/formatting should be applied first (in inner scope)
