2f8ed3278aa590e4977a294f26fd9acd84f69816
COTHSC <thierryscully.colomban@gmail.com>
2026-05-11 10:02:58 +0200

    feat: add DOCLINGCORE_ALLOWED_PRIVATE_IPS setting to allow private IP access
    Signed-off-by: COTHSC <thierryscully.colomban@gmail.com>

--- docling_core/utils/settings.py
+++ docling_core/utils/settings.py
@@ -6,6 +6,7 @@ class CoreSettings(BaseSettings):
 
     allow_image_file_uri: bool = False
     max_image_decoded_size: int = 20 * 1024 * 1024  # 20MB
+    allowed_private_ips: list[str] = []
 
     # DocLang deserialize budgets (DoS protection for untrusted markup / .dclx)
     max_doclang_xml_bytes: int = 128 * 1024 * 1024  # 128 MiB
