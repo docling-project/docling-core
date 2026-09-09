17f1a958affeed3d42a314681387f02614ccd6bb
Luciano Sant'Ana <lucsantana@gmail.com>
2026-09-09 06:44:48 -0300

    fix: register AFP MIME types (#761)
    Signed-off-by: Luciano Sant'Ana <lucsantana@gmail.com>

--- docling_core/types/doc/common/origin.py
+++ docling_core/types/doc/common/origin.py
@@ -31,6 +31,7 @@ class DocumentOrigin(BaseModel):
         "application/vnd.apple.keynote",
         "application/vnd.box.boxnote",
         "application/vnd.docling.ebcdic",  # accepted alias, see application/x-ebcdic
+        "application/vnd.ibm.modcap",
         "application/vnd.oasis.opendocument.presentation",
         "application/vnd.oasis.opendocument.spreadsheet",
         "application/vnd.oasis.opendocument.text",
@@ -43,6 +44,7 @@ class DocumentOrigin(BaseModel):
         # EBCDIC data files have no IANA-registered media type, so they will never
         # appear in mimetypes.types_map. "application/x-ebcdic" is the standard
         # spelling docling emits; the vendor-tree alias above is accepted as well.
+        "application/x-afp",
         "application/x-ebcdic",
         "application/x-iwork-pages-sffpages",
         "application/x-iwork-numbers-sffnumbers",
